272c0aa594ac7cd07d4e8c427035e90e1d2dacdd
Vijay199205 <vijayamogaveera100@gmail.com>
2025-10-30 06:49:35 +0530

    new changes

---
+++ App.js
@@ -0,0 +1,15 @@
+ 
+            const parent = React.createElement("div", {id:"parent"},
+                [ React.createElement("div", {id:"child"},[ React.createElement("h1", {},"Hello" ), React.createElement("h2", {},"Hello2" )]),
+            
+             React.createElement("div", {id:"child"},[ React.createElement("h1", {},"Hello" ), React.createElement("h2", {},"Hello2" )])])
+ 
+ 
+ //const heading = React.createElement("h1", {id:"heading", className:"vijay"}, "Hello my react page");
+ console.log(parent)
+            const root = ReactDOM.createRoot(document.getElementById('root'));
+            // console.log(root);
+            // console.log( root.render(heading));
+            root.render(parent);
+
+
