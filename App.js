edd388a5f4809688d90d969cf75f834d47ba5c25
Vijay199205 <vijayamogaveera100@gmail.com>
2025-10-30 06:52:03 +0530

    new add

--- App.js
+++ App.js
@@ -4,7 +4,6 @@
             
              React.createElement("div", {id:"child"},[ React.createElement("h1", {},"Hello" ), React.createElement("h2", {},"Hello2" )])])
  
- 
  //const heading = React.createElement("h1", {id:"heading", className:"vijay"}, "Hello my react page");
  console.log(parent)
             const root = ReactDOM.createRoot(document.getElementById('root'));
